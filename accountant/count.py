5de965896c1e9d19d7cbcf4ac67cd61e2a18149b
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2022-12-31 23:24:33 -0500

    Update count.py
    The whitespace handler doesn't work the way I want it to. The else condition kept firing. So I removed the handler.

--- accountant/count.py
+++ accountant/count.py
@@ -14,93 +14,6 @@ Example:
 45.78   Deposit
 -6.82   Withdrawal
 
-
-Core is script to add positive and negative numbers. Base number is 0.
-
-
-227pm
-Error thrown: TypeError: unsupported operand type(s) for +=: 'int' and '_io.TextIOWrapper'
-Answer - https://astrofrog.github.io/py4sci/_static/08.%20Reading%20and%20writing%20files.html
-    variable f thats assigned to file open func is a file handle. file open func is just
-    returning an accessible object.
-
-    for line in f:
-        print(repr(line)) #shows contents line by line in strings; includes \n chars
-
-        #str(line) works too. Doesn't show \n characters. Use this.
-
-        #int(line) doesn't work.
-
-Additions:
-Filter out + and - . Integers only
-
-406pm
-I think the program worked. It returned $93.93.
-
-45.67 + 45.78 + 5.89 - 3.41 = 93.93
-
-The program works!
-
-546pm
-Broke the program. Got stuck on how my own code worked, because I copied and pasted some code from
-the internet
-
-552pm
-Fixed.
-
-1039pm
-Importing regular expressions library for proper format
-
-1103pm
-Now I'm finished. Proper format established.
-
-Actually, not done. Didn't take to account the amounts with 0 cents; no decimal point
-
-1108pm
-Updated.
-
-1109pm
-Final test passed. This program is ready now.
-
-#####
-631pm 12/25/2022
-
-Updating v1. Adding spaces fires the else condition in the for loop that prompts to the user
-that he/she is typing the payment wrong. The if condition can't find a match from whitespace.
-Will add an elif condition to handle whitespace.
-
-643pm
-Updated. Now to figure out how to handle the float problem. Sometimes I get answers that include 13 decimal places
-to the right.
-
-651pm
-Added round functions in the match case block. round(value, digits after decimal pt ). Hope it works. Do I import
-the math library?
-
-Not working.
-
-740pm
-Solved the problem. Didnt need the round functions at all. Inside the match cases, take the string input and make
-that into a float. Then multiply by 100 to get rid of the decimal point. Finally, when the for loop finishes
-reading the file line by line, divide the total amount by 100 to bring the decimal point back.
-
-v1.1 complete
-
-834pm
-Updated the if statement regex to handle writing in cents. 6 cents: 0.06, .06
-+ to * for first 2 alternations.
-
-v1.2 done
-
-1007pm
-The newline characters were causing the prompt in the else condition to not display properly. The badly written
-input would be displayed, but the rest of the prompt got displayed on the next line. Not what I want.
-
-Assigned a variable named transaction to hold inputs without \n.
-
-v1.3
-
-
 """
 import re
 
@@ -137,14 +50,10 @@ for line in f:     #iterate each line from file
             case _: 
                 total += float(transaction) * 100       #Remove decimal point   
     
-    elif re.compile(r"^\s$").findall(transaction):      #whitespace handling
+    else: 
         
         pass   #skip line and continue
-    
-    else:
-        
-        print(f"{transaction} is not a valid amount. Skipping this.")
-        
+ 
 total /= 100    #back to decimal point
     
 print(f"Total is: ${format(total,'.2f')}")  #show total amount with 2 decimal points to the right
